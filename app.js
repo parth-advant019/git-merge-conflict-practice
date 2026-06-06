c5ff4195395f25316b3c6c65dfe35d6dc580ad68
parth <parth@advant.xyz>
2026-06-06 16:51:33 +0530

    chang in main again

--- app.js
+++ app.js
@@ -11,6 +11,6 @@ const addTO10 = adder(10);
 addTO10(5);
 addTO10(7);
 
-console.log("welcome from third branch changed code");
+console.log("welcome from third branch changed code i change in main");
 console.log("code from main again");
 console.log("code from third branch");
